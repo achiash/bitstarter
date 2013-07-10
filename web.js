7171ce4b599017b2ac1c942e30ec1db954b7db05
Achia Shaag <achia.shaag@gmail.com>
2013-07-10 20:21:52 +0000

    changes to web.js

--- web.js
+++ web.js
@@ -3,6 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  fs.readFile('/etc/passwd', function (err, data) {
+     if (err) throw err;
+     console.log(data);
+     response.send
+  });
   response.send('Hello World 2!');
 });
 
@@ -10,3 +15,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
